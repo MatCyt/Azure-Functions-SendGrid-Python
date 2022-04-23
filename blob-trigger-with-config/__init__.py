cc7fe7aaa11374446e18bc01d5f52630e2d8f233
MatCyt <mateusz.cytrowski@gmail.com>
2022-04-23 19:04:08 +0200

    added az function for blob trigger with config

--- blob-trigger-with-config/__init__.py
+++ blob-trigger-with-config/__init__.py
@@ -1,4 +1,5 @@
 import os
+import json
 import logging
 import base64
 
@@ -17,12 +18,25 @@ def main(myblob: func.InputStream):
     connection_string = os.environ["STORAGE_CONNECTION_STRING"]
     sendgrid_api_key = os.environ["SENDGRID_API_KEY"]
 
-    # create email object with defined parameters
-    sender_email = os.environ["SENDER_EMAIL"]
-    receipent_email = os.environ["RECEIPENT_EMAIL"]
-    email_subject = "Your email subject goes here"
-    email_content = "And email content goes here"
+    # load email details for specific blob based on blob folder
+    config_path = os.path.join('blob-trigger-with-config', 'config.json')
 
+    with open(config_path) as json_file:
+        email_details = json.load(json_file)
+
+    try:
+        container_name, data_provider, file_name = myblob.name.split('/')
+        blob_name = myblob.name.split('/', 1)[1]
+
+        sender_email = email_details[data_provider]['sender_mail']
+        receipent_email = email_details[data_provider]['receipent_mail']
+        email_subject = email_details[data_provider]['subject']
+        email_content = email_details[data_provider]['content']
+
+    except Exception as e:
+        logging.error(f'new blob in incorrect file path or data provider not found in config file - error message: {str(e)}')
+
+    # create email object
     mail = Mail(
         from_email=Email(sender_email),
         to_emails=To(receipent_email),
@@ -31,8 +45,6 @@ def main(myblob: func.InputStream):
 
     # load blob as stream and create attachment object
     try:
-        container_name, blob_name = myblob.name.split('/')
-
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         blob_client = blob_service_client.get_blob_client(container_name, blob_name)
         stream_object_from_blob = blob_client.download_blob()
@@ -42,7 +54,7 @@ def main(myblob: func.InputStream):
         encoded = base64.b64encode(stream.getvalue()).decode()
         attachment = Attachment()
         attachment.file_content = FileContent(encoded)
-        attachment.file_name = FileName(blob_name)
+        attachment.file_name = FileName(file_name)
         attachment.disposition = Disposition('attachment')
         attachment.content_id = ContentId('Example Content ID')
         mail.attachment = attachment
